3001b17661add84ab6f302ca9e59e3b1cef08a9a
SuhasMarade <suhasmarade99@gmail.com>
2023-08-31 21:28:38 +0530

    image added in assets

--- userpage/src/Logo/Logo.jsx
+++ userpage/src/Logo/Logo.jsx
@@ -1,7 +1,8 @@
 import "./Logo.css"
+
 function Logo () {
     return (
-        <img src="https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png" height="100px" width="300px"/>
+        <img src="src/assets/instagramTextLogo.png" alt="text-logo" height="100px" width="300px"/>
         
     );
 }
